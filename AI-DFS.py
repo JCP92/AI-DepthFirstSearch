5314716afa98120c6974e4971a258e97466c95e7
Joshua Peckham <joshpeck_11@hotmail.com>
2019-02-08 17:16:18 -0800

    Update AI-DFS.py
    Refactored code and corrected printing process to show the board, moves and how many turns.

--- AI-DFS.py
+++ AI-DFS.py
@@ -1,22 +1,14 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
-#implementation of function "Iterative_deepening_DFS"
+#implementation of function "DFS"
 import time
 import numpy as np
 import sys
-
 goalState = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-'''results = []
-iterations = []
-moves = []
-steps = []
-timer = []
-dLimit = 10''' #use at other time
-
+que = []
+quePopper = 0
+queMaxCounter = 0
+queNodeDepth = [1]
+pathCost = [0]
+visited = []
 
 
 
@@ -29,20 +21,15 @@ class Node(object):
         self.left = None
         self.up = None
         self.down = None
-        self.right = None 
+        self.right = None
 
     def DFS(self):
         start = time.time()
+        global que, quePopper, queMaxCounter, queNodeDepth, pathCost, visited
+        que.insert(0, self) #initialization of que
 
-        que = [self] #initialization of que
-        quePopper = 0 #how many pops are made
-        queMaxCounter = 1 
 
-        queNodeDepth = [0] #how deep it goes ;P
-        pathCost = [0] 
-        visited = [] #prevent repeated paths
-        
-        while que is not None: 
+        while que is not None:
             if len(que) > queMaxCounter:
                 queMaxCounter = len(que)
             curNode = que.pop(0)
@@ -51,76 +38,83 @@ class Node(object):
             curDepth = queNodeDepth.pop(0) #take from queue
             curPathCost = pathCost.pop(0) #initialize path cost -> use later
             visited.append(curNode.state.copy()) #adds all visited items that are unique
-            
+
             print('Starting: ')
             print(curNode.state)
-            if curNode.compState(curNode.state):
-                curNode.printBoard() #check coding
+            if np.array_equal(curNode.state, goalState):
+                curNode.printPathway()
                 print('Time: ', str(quePopper), ' nodes popped off the queue')
                 print('Space: ', str(queMaxCounter), ' nodes in the queue at its max')
                 print('time spent: %0.2fs' % (time.time() - start))
                 return true
             else:
-                if (curNode.swapCheck(curNode.state, -1)): #expand left node
-                    curNode.left = Node(curNode.doSwap(curNode.state, -1), curNode.state, 'l', curNode.depth + 1)
-                    if (curNode.left.state not in visited): #store if not visited
-                        que.insert(0, curNode.left)
-                        queNodeDepth.insert(0, curNode.left.depth)
-                        pathCost.insert(0, (curNode.state[curNode.state.index(0) - 1] + curPathCost))
-                        print("L: " + str(curNode.left.state))
+                curNode.left = curNode.expandNode(-1, 'l', curPathCost)
+                curNode.right = curNode.expandNode(1, 'r', curPathCost)
+                curNode.up = curNode.expandNode(-3, 'u', curPathCost)
+                curNode.down = curNode.expandNode(3, 'd', curPathCost)
 
-                if (curNode.swapCheck(curNode.state, 1)): #expand right node
-                    curNode.right = Node(curNode.doSwap(curNode.state, 1), curNode.state, 'r', curNode.depth + 1)
-                    if (curNode.right.state not in visited):
-                        que.insert(0, curNode.right)
-                        queNodeDepth.insert(0, curNode.right.depth)
-                        pathCost.insert(0, (curNode.state[curNode.state.index(0) + 1] + curPathCost))
-                        print("R: " + str(curNode.right.state))
+def expandNode(self, val, direction, curPathCost):
+    if (self.swapCheck(val)): #expand node
+        pntr = Node(self.doSwap(val), self.state, direction, self.depth + 1)#store expanded node
+        if (pntr.state not in visited): #add to ques if not visited
+            que.insert(0, pntr)
+            queNodeDepth.insert(0, pntr.depth)
+            pathCost.insert(0, (self.state[self.state.index(0) + val] + curPathCost))
+            print(direction + ": " + str(pntr.state))
+            return pntr;
+        else:
+            return None
 
-                if (curNode.swapCheck(curNode.state, -3)) : #expand up node
-                    curNode.up = Node(curNode.doSwap(curNode.state, -3), curNode.state, 'u', curNode.depth + 1)
-                    if (curNode.up.state not in visited):
-                        que.insert(0, curNode.up)
-                        queNodeDepth.insert(0, curNode.up.depth)
-                        pathCost.insert(0, (curNode.state[curNode.state.index(0) - 3] + curPathCost))
-                        print("U: " + str(curNode.up.state))
+def swapCheck(self, val): #verify if able to swap
+    index = self.state.index(0)
+    move = index + val
+    if (0 <= move <= 8):
+        if val == -1:#check left
+            if (index != 0) & (index != 3) & (index != 6):
+                return True
+        elif val == 1: #check right
+            if (index != 2) & (index != 5) & (index != 8):
+                return True
+        elif val == -3: #check up
+            if (index != 0) & (index != 1) & (index != 2):
+                return True
+        elif val == 3: #check down
+            if (index != 6) & (index != 7) & (index != 8):
+                return True
+return False
 
-                if (curNode.swapCheck(curNode.state, +3)): #expand down node
-                    curNode.down = Node(curNode.doSwap(curNode.state, +3), curNode.state, 'd', curNode.depth + 1)
-                    if (curNode.down.state not in visited):
-                        que.insert(0, curNode.down)
-                        queNodeDepth.insert(0, curNode.down.depth)
-                        pathCost.insert(0, (curNode.state[curNode.state.index(0) + 3] + curPathCost))
-                        print("D: " + str(curNode.down.state))
-           
-    def swapCheck(self, curState, val): #verify if able to swap
-        index = curState.index(0)
-        move = index + val
-        if (0 <= move <= 8):
-            if val == -1:#check left
-                if (index != 0) & (index != 3) & (index != 6):
-                    return True
-            elif val == 1: #check right
-                if (index != 2) & (index != 5) & (index != 8):
-                    return True
-            elif val == -3: #check up
-                if (index != 0) & (index != 1) & (index != 2):
-                    return True
+def doSwap(self, val): #swap it
+    copy = self.state.copy()
+    index = self.state.index(0)
+    copy[index], copy[index + val] = copy[(index + val)], copy[index]
+    return copy
 
-            elif val == 3: #check down
-                if (index != 6) & (index != 7) & (index != 8):
-                    return True
-        return False
-    def doSwap(self, nState, val): #swap it
-        copy = nState.copy()
-        index = nState.index(0)
-        copy[index], copy[index + val] = copy[(index + val)], copy[index]
-        return copy
-    def compState(self, state): #is it goals or nah?
-        for i in range (9):
-            if state[i] != goalState[i]:
-                return False
-        return True
+def printBoard(self, state):
+    if curState == None:
+        print("NONE")
+    else:
+        print("-------------")
+        print("| %i | %i | %i |" % (state[0], state[1], state[2]))
+        print("-------------")
+        print("| %i | %i | %i |" % (state[3], state[4], state[5]))
+        print("-------------")
+        print("| %i | %i | %i |" % (state[6], state[7], state[8]))
+        print("-------------")
+
+def printPathway(self):
+    statePath = self.state
+    directionPath = self.operator
+    depthPath = self.depth
+    i = 0
+    while (self.parent):
+        self = self.parent
+        statePath.append(self.state)
+        directionPath.append(self.operator)
+        depthPath.append(self.depth)
+    while statePath:
+        print("Step: " + i + "\tDirection: " + directionPath.pop() +"\tDepth: " + depthPath.pop())
+        self.printBoard(statePath.pop())
+
 
 def IDS (inState): #incomplete
     for i in range ( dLimit ):
@@ -130,13 +124,13 @@ def IDS (inState): #incomplete
         results.append(root.state)
         DFS(root, i)
         t1 = time.time() - t
-        timer.append(t1) 
-        
+        timer.append(t1)
+
 
 def main():
     f = open("8puz.txt", "r")
-          
-    for n in range(1): 
+
+    for n in range(1):
         line = f.readline().split(', ')
         line =[int(i) for i in line]
         inState = line
@@ -147,13 +141,3 @@ def main():
     f.close()
 
 main()
-
-
-# # Assignment 1, 8-Puzzle Programming
-
-# # Your homework must be implemented in this Notebook file. 
-# You can add as many cells as you want. However, you are not allowed to touch the code below the line "=============".
-# You need to implement the three (four for grads) searching functions and the print result functions.
-# For the searching functions, feel free to customize the return data types and parameter lists as long as the function name is as required.
-# 
-
